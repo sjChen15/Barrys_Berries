a60fd60e04b4ceb21416426e94cdb4e715894fec
Shijia Chen <shijiajchen@gmail.com>
2022-08-03 12:48:42 -0400

    added head to head and worse dfs

--- src/logic.py
+++ src/logic.py
@@ -1,4 +1,6 @@
-import random, numpy
+from curses.panel import top_panel
+from re import L, U
+from turtle import left, right
 from typing import List, Dict
 
 def get_info() -> dict:
@@ -18,18 +20,14 @@ def choose_move(data: dict) -> str:
     my_head = (head_x,head_y) #tuple with coordinates
     my_body = my_snake["body"]  
   
-    food = data['board']['food']
-  
     board = data['board']
     board_height = board["height"]
     board_width = board["width"]
+    food = board['food']
 
     grid = [[0 for x in range(board_width)] for y in range(board_height)]
-    snake_heads = [] #list of heads of snakes
-    fill_snakes(grid,snake_heads,board["snakes"])
+    fill_snakes(grid, board["snakes"])
     #print_grid(grid)
-    snake_heads.remove(my_snake["head"])
-    print(f"Heads of snakes: {snake_heads}")
   
     possible_moves = ["up", "down", "left", "right"] 
     
@@ -50,16 +48,19 @@ def choose_move(data: dict) -> str:
 
     print(f"Legal moves: {legal_moves}")
 
-    #best moves by bsf
+    #best moves by dfs with dumb dfs
     serach_moves = dfs_moves(legal_moves,grid,my_head)
     print(f"dfs_moves: {serach_moves}")
-
+    
     #find potential head to head hits
-    head_moves = head_to_head(legal_moves,snake_heads)
+    other_snakes = data["snakes"].remove(my_snake)
+    if(len(other_snakes) !=0):
+      head_moves = head_to_head(legal_moves,other_snakes,my_snake)
+
     #closest food
     food_moves = moves_to_food(food,legal_moves,my_head)
   
-    move = best_move(legal_moves,serach_moves,food_moves,my_snake)
+    move = best_move(legal_moves,serach_moves[0],serach_moves[1],head_moves,food_moves,my_snake)
     print(f"MOVE {data['turn']}: {move} picked from all valid options in {legal_moves}")
     print()
   
@@ -72,17 +73,15 @@ def print_grid(grid:List[List[int]])->None:
     print(grid[l-i-1])
 
     
-def fill_snakes(grid:List[List[int]],heads:List[dict],snakes: dict) -> None:
+def fill_snakes(grid:List[List[int]],snakes: dict) -> None:
   for s in snakes:
     body = s["body"] #list of coords
     for pos in body:
       #print(pos)
-      if pos == body[0]: #head of snake
-        heads.append(pos)
       grid[ pos["y"] ][ pos["x"] ] = 1
 
 #return moves in order of which has most space up to size of snake
-def dfs_moves(moves:List[dict], grid: List[List[int]], head:tuple) -> dict:
+def dfs_moves(moves:List[dict], grid: List[List[int]], head:tuple) -> List[dict]:
   x=head[0]
   y=head[1]
 
@@ -92,26 +91,37 @@ def dfs_moves(moves:List[dict], grid: List[List[int]], head:tuple) -> dict:
     "right":0,
     "left":0
   }
-  
+  dumb_count = {
+    "up":0,
+    "down":0,
+    "right":0,
+    "left":0
+  }
+
   for m in count:
     if m not in moves:
-      
       continue
     looked = [[0 for x in range(len(grid))] for y in range(len(grid))]
+    dumb_looked= [[0 for x in range(len(grid))] for y in range(len(grid))]
     if m=="up":
       print(f"counting for {m}")
       count[m] = dfs((x+1,y),grid,looked)
+      dumb_count[m] = dumb_dfs((x+1,y),grid,dumb_looked,m)
     if m=="down":
       print(f"counting for {m}")
       count[m] = dfs((x-1,y),grid,looked)
+      dumb_count[m] = dumb_dfs((x-1,y),grid,dumb_looked,m)
     if m=="left":
       print(f"counting for {m}")
       count[m] = dfs((x,y-1),grid,looked)
+      dumb_count[m] = dumb_dfs((x,y-1),grid,dumb_looked,m)
     if m=="right":
       print(f"counting for {m}")
       count[m] = dfs((x,y+1),grid,looked)
+      dumb_count[m] = dumb_dfs((x,y+1),grid,dumb_looked,m)
 
-  return dict(sorted(count.items(), key=lambda item: item[1], reverse=True))
+  ret = [dict(sorted(count.items(), key=lambda item: item[1], reverse=True)),dict(sorted(dumb_count.items(), key=lambda item: item[1], reverse=True))]
+  return ret
 
 def dfs(p:tuple,grid:List[List[int]],looked:List[List[int]]) ->int:
   l=len(grid)
@@ -142,12 +152,90 @@ def dfs(p:tuple,grid:List[List[int]],looked:List[List[int]]) ->int:
   
   return sum
 
+def dumb_dfs(p:tuple,grid:List[List[int]],looked:List[List[int]],m:str) ->int:
+  l=len(grid)
+  if p[0] >=l or p[1] >=l or p[0] <0 or p[1]<0:
+    #print("here 1")
+    return 0
+  if looked[p[0]][p[1]] ==1:
+    #print("here 2")
+    return 0
+  if grid[p[0]][p[1]] == 1:
+    #print("here 3")
+    return 0
+    
+  looked[p[0]][p[1]] = 1
+  
+  #see if up down left and right have been looked for/in range
+  look_up = 1 if(p[1]+1 < l and looked[p[0]][p[1]+1] == 0) else 0
+  look_down = 1 if(p[1]-1 >= 0 and looked[p[0]][p[1]-1] == 0) else 0
+  look_right = 1 if (p[0]+1 < l and looked[p[0]+1][p[1]] == 0) else 0
+  look_left = 1 if(p[0]-1 >=0 and looked[p[0]-1][p[1]] == 0) else 0
+  
+  sum = 1
+  if m=="up":
+    sum += dfs((p[0],p[1]+1),grid,looked,m) if look_up else 0
+    sum += dfs((p[0]+1,p[1]),grid,looked,m) if look_right else 0
+    sum += dfs((p[0]-1,p[1]),grid,looked,m) if look_left else 0  
+  elif m=="down":
+    sum += dfs((p[0],p[1]-1),grid,looked,m) if look_down else 0
+    sum += dfs((p[0]+1,p[1]),grid,looked,m) if look_right else 0
+    sum += dfs((p[0]-1,p[1]),grid,looked,m) if look_left else 0  
+  elif m=="left":
+    sum += dfs((p[0],p[1]+1),grid,looked,m) if look_up else 0
+    sum += dfs((p[0],p[1]-1),grid,looked,m) if look_down else 0
+    sum += dfs((p[0]-1,p[1]),grid,looked,m) if look_left else 0  
+  elif m=="right":
+    sum += dfs((p[0],p[1]+1),grid,looked,m) if look_up else 0
+    sum += dfs((p[0],p[1]-1),grid,looked,m) if look_down else 0
+    sum += dfs((p[0]+1,p[1]),grid,looked,m) if look_right else 0
+  
+  return sum
+
 #returns list of legal moves that don't have bad head to head collisions
-def head_to_head(moves:List[dict],heads: List[dict]) -> List[str]:
+def head_to_head(moves:List[dict],snakes: List[dict],my_snake:dict) -> List[str]:
 #find possible head to head collisions and only remove if the snake is >= 
-  return ["up"]
+#check 3x3 space around head
+  heads = []
+  for s in snakes:
+    heads.append(s["body"][0])
+  
+  print(f"Heads: {heads}")
 
+  #up0, left1, right2, down3, up_left4, up_right5, down_left6, down_right7
+  x=my_snake["head"]["x"]
+  y=my_snake["head"]["y"]
+  checks = [
+    {"x":x,"y":y+2},
+    {"x":x-2,"y":y},
+    {"x":x+2,"y":y},
+    {"x":x,"y":y-2},
+    {"x":x-1,"y":y+1},
+    {"x":x+1,"y":y+1},
+    {"x":x-1,"y":y-1},
+    {"x":x+1,"y":y-1},
+  ]
+  hits=[]
 
+  for m in moves:
+    if m == "up":
+      move_check = [checks[0],checks[4],checks[5]]
+    elif m == "left":
+      move_check  = [checks[1],checks[4],checks[6]]
+    elif m == "right":
+      move_check = [checks[2],checks[5],checks[7]]
+    elif m == "down":
+      move_check = [checks[3],checks[6],checks[7]]
+
+    for s in snakes:
+      head=s["body"][0]
+      if head in move_check and s["length"]>=my_snake["length"]:
+        hits.append(m)
+        continue
+  
+  print(f"Potential head to heads: {hits}")
+
+  return hits
   
 #returns moves that move closest to food
 def moves_to_food(food:List[dict],moves:List[dict],head:tuple)->List[str]:
@@ -178,22 +266,17 @@ def moves_to_food(food:List[dict],moves:List[dict],head:tuple)->List[str]:
   return (ideal_moves)
 
 #bfs to find move with most space up to its own size and closer to food if possible
-def best_move(moves:List[str], bfs:dict, food:List[str],me:dict)->str:
+def best_move(moves:List[str], dfs:dict, dumb_dfs:dict,head_hits:List[str],food:List[str],me:dict)->str:
   #go through bfs and add smallest value of bfs val but move present in moves
   best=[]
-  for s_m in bfs:
-    if s_m in moves:
-      if bfs[s_m] >= me["length"]:  
-        best.append(s_m)
 
-  #no spaces bigger than snake size
-  if(len(best) == 0):
-    for s_m in bfs:
-      if s_m in moves:
-        if len(best) == 0:
-          best.append(s_m)
-        elif best[0] == bfs[s_m]:  
-          best.append(s_m)
+  #get best values where they are largest values possible
+  for s_m in dfs:
+    if s_m in moves:
+      if len(best) == 0:
+        best.append(s_m)
+      elif best[0]==dfs[s_m]:  
+        best.append(s_m)
           
   print(f"Best moves: {best}")
   #now we have a list of all smallest values
@@ -203,11 +286,29 @@ def best_move(moves:List[str], bfs:dict, food:List[str],me:dict)->str:
   best_move = best[0] #default
   if(len(best) == 1):
     return best_move
-    
-  #look into future for each of the best moves and choose 
-  #elif:
-    #for
 
+  #remove head to heads if there are any
+  no_head_hits=best
+  if(len(head_hits)!=0):
+    for h in head_hits:
+      if(h in best):
+        no_head_hits.remove(h)
+
+    if(len(no_head_hits) != 0):
+      best=no_head_hits
+      best_move = best[0]
+
+  if(len(best) == 1):
+    return best_move
+  
+  #use worse bfs to determine which direction out of 2 is worse
+  if(len(best) == 2):
+    if(dumb_dfs[best[0]] > dumb_dfs[best[1]] ):
+      return best[0]
+    else:
+      return best[1]
+
+    
 
   #if for 1+ check if one is in food, return that one
   for m in best:
